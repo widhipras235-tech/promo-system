298a24f098f78f3770383c373d454b939b952dc0
Widhi Pras <widhipras235@gmail.com>
2026-03-14 04:41:05 +0700

    update app.js

--- app/app.js
+++ app/app.js
@@ -1,3 +1,63 @@
+let DB=[]
+let fuse
+
+const result=document.getElementById("result")
+const searchInput=document.getElementById("search")
+
+async function loadDatabase(){
+
+DB = await fetch("../db/promo.json").then(r=>r.json())
+
+fuse = new Fuse(DB,{
+keys:["artikel","deskripsi"],
+threshold:0.3
+})
+
+}
+
+window.onload=loadDatabase
+
+
+searchInput.addEventListener("keyup", e=>{
+
+let q=e.target.value.trim()
+
+if(q.length<3) return
+
+search(q)
+
+})
+
+
+function search(q){
+
+q=q.toUpperCase()
+
+const divisi=document.getElementById("divisi").value
+
+let data=[]
+
+// SKU exact
+data=DB.filter(x=>x.sku==q)
+
+// artikel fuzzy
+if(data.length==0){
+
+data=fuse.search(q).map(x=>x.item)
+
+}
+
+// filter divisi
+if(divisi){
+
+data=data.filter(x=>x.division===divisi)
+
+}
+
+render(data)
+
+}
+
 function render(data){
 
 if(data.length==0){
@@ -110,5 +170,28 @@ html+=`
 })
 
 result.innerHTML=html
+
+}
+
+
+function scan(){
+
+const scanner=new Html5Qrcode("reader")
+
+scanner.start(
+{facingMode:"environment"},
+{fps:10,qrbox:250},
+
+barcode=>{
+
+document.getElementById("search").value=barcode
+
+search(barcode)
+
+scanner.stop()
+
+}
+
+)
 
 }
