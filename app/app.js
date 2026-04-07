56013fa728387fcca6ce971835d990283245b0a1
widhipras235-tech <widhipras235@gmail.com>
2026-04-07 14:42:03 +0700

    Update app.js

--- app/app.js
+++ app/app.js
@@ -19,6 +19,7 @@ const statusEl = document.getElementById("status")
 const btnCamera = document.getElementById("btnCamera")
 const video = document.getElementById("camera")
 const canvas = document.getElementById("canvas")
+overlay.id = "overlay"
 const ctx = canvas.getContext("2d")
 
 const scanFrame = document.getElementById("scanFrame")
